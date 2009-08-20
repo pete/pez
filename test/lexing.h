6503a5f7a7aa9e584ac076f2b3e31a9b407563ce
Matthew King <automatthew@gmail.com>
2009-08-20 18:15:20 -0500

    hook in the lexing suite

--- test/lexing.h
+++ test/lexing.h
@@ -5,8 +5,26 @@
 
 char msg[50]; // for when you want to sprintf stuff
 
-static char* test_something() {
+// declare, maybe link will work?
+int get_quoted_string(char **strbuf, char token_buffer[]);
+
+static char* test_quoted_string() {
+	int valid;
+	int i;
+	char *input[150];
+	*input = "\"John Bigboote\"";
+	char token_buffer[128];
+	valid = get_quoted_string(input, token_buffer);
+	mu_assert("String should be valid", valid);
+	mu_assert("String different?", strcmp(token_buffer, "John Bigboote") == 0);
 	
+	*input = "\"\"";
+	valid = get_quoted_string(input, token_buffer);
+	mu_assert("Empty string should be valid", valid);
+	
+	*input = "\"";
+	valid = get_quoted_string(input, token_buffer);
+	mu_assert("Unterminated string should be invalid", !valid);
 	
 	return 0;
 }
@@ -14,7 +32,7 @@ static char* test_something() {
 char* lexing_suite() {
 	pez_init(); // Usually necessary.
 	
-	mu_run_test(test_something);
+	mu_run_test(test_quoted_string);
 
 	return 0;
 }
--- test/test_all.c
+++ test/test_all.c
@@ -1,5 +1,6 @@
 #include "api.h"
 #include "eval_functions.h"
+#include "lexing.h"
 #include "minunit.h"
 
 int tests_run = 0;
@@ -8,6 +9,7 @@ int tests_run = 0;
 int main(int argc, char **argv) {
 	mu_run_suite(api_suite);
 	mu_run_suite(eval_functions_suite);
+	mu_run_suite(lexing_suite);
 
 	printf("ALL TESTS PASSED. %d tests run.\n", tests_run);
 
